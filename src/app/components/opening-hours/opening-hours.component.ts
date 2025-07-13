4a727305dd36d8b58755d799920d7f1f956fd061
dihcoder <mdsidiego@gmail.com>
2025-07-13 19:24:55 -0300

    feat: Opening Hours section

--- src/app/components/opening-hours/opening-hours.component.ts
+++ src/app/components/opening-hours/opening-hours.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-opening-hours',
-  imports: [],
-  templateUrl: './opening-hours.component.html',
-  styleUrl: './opening-hours.component.scss'
+	selector: 'app-opening-hours',
+	imports: [],
+	templateUrl: './opening-hours.component.html',
+	styleUrl: './opening-hours.component.scss'
 })
 export class OpeningHoursComponent {
-
+	hours = [
+		{ day: 'Segunda a Sexta', time: '10h - 20h30', isClosed: false },
+		{ day: 'Sábado', time: 'Fechado', isClosed: true },
+		{ day: 'Domingo', time: '10h – 20h30', isClosed: false }
+	];
 }
