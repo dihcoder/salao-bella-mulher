597cf89d799c83ac4963c0d7010206b46d01f4a0
dihcoder <mdsidiego@gmail.com>
2025-07-14 16:10:54 -0300

    hotfix: Use BRL currency in Featured Services section cards

--- src/app/components/featured-services/featured-services.component.ts
+++ src/app/components/featured-services/featured-services.component.ts
@@ -1,8 +1,9 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-featured-services',
-    imports: [],
+    imports: [CurrencyPipe],
     templateUrl: './featured-services.component.html',
     styleUrl: './featured-services.component.scss'
 })
@@ -12,19 +13,19 @@ export class FeaturedServicesComponent {
             icon: '/barbershop.svg',
             name: 'Corte Feminino',
             description: 'Realce sua beleza com um corte moderno e feito sob medida para o seu estilo e personalidade. Técnicas atuais, com atenção aos detalhes e acabamento impecável.',
-            price: 49
+            price: 49.0
         },
         {
             icon: '/flask-round-potion.svg',
             name: 'Hidratação Profunda',
             description: 'Devolva o brilho, a maciez e a vitalidade dos fios com um tratamento intensivo que nutre profundamente, ideal para cabelos ressecados, quimicamente tratados ou opacos.',
-            price: 65
+            price: 65.0
         },
         {
             icon: '/hairbrush.svg',
             name: 'Escova Modelada',
             description: 'Fios alinhados, com volume e movimento na medida certa. A escova ideal para um evento especial ou para se sentir ainda mais linda no dia a dia.',
-            price: 40
+            price: 40.0
         }
     ];
 }
